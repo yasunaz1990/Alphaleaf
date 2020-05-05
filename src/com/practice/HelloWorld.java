51bf97cdf7b8f59bfd4fc3fd176bf7eda6ac71b6
Nijat Muhtar <yasunaz1990@gmail.comyasunaz1990@gmail.comyasunaz1990@gmail.com>
2020-05-05 18:59:39 -0400

    made progress bar function

--- src/com/practice/HelloWorld.java
+++ src/com/practice/HelloWorld.java
@@ -2,8 +2,8 @@ package com.practice;
 
 public class HelloWorld {
 
-    // Main method
+    // Main method - where CPU executes code
     public static void main(String[] args) {
-
+        System.out.println("Hello World!!");
     }
 }
