b1fb6c4d42028c51fcf9cf4c71e7b32d0f08ca7f
Anna <amholmes1300@gmail.com>
2020-05-05 22:04:20 -0400

    added new line

--- src/com/practice/HelloWorld.java
+++ src/com/practice/HelloWorld.java
@@ -6,5 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
         System.out.println("Hello Nijat!!");
+        System.out.println("Hello Anna!!");
+
     }
 }
