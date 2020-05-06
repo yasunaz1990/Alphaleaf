d9827683322d91c9a7a888e3f6f3954ae55a7fe7
ZOEY <zhebron09@gmail.com>
2020-05-05 22:04:24 -0400

    added new line

--- src/com/practice/HelloWorld.java
+++ src/com/practice/HelloWorld.java
@@ -1,9 +1,10 @@
 package com.practice;
 
 public class HelloWorld {
-
+cl
     // Main method - where CPU executes code
     public static void main(String[] args) {
         System.out.println("Hello World!!");
     }
+
 }
