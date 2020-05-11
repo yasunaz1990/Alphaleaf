e7f13a5659bfb096aa21f187b90ec07639038722
Nijat Muhtar <yasunaz1990@gmail.comyasunaz1990@gmail.comyasunaz1990@gmail.com>
2020-05-11 19:06:14 -0400

    some clean up

---
+++ src/com/variable/Operators.java
@@ -0,0 +1,18 @@
+package com.variable;
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+        // 1. We are able to represent a data and store it into variable.
+        // 2. Manipulation of these stored data
+        String myName = "nijat murazzim muhtar";
+
+        // Operation
+        System.out.println( myName );
+
+
+        // Manipulate
+        System.out.println( myName.toUpperCase() );
+    }
+}
