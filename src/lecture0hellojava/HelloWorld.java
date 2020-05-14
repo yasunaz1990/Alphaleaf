efdf91ab5d962e0a48c446739c4b98068b98fd92
Nijat Muhtar <yasunaz1990@gmail.comyasunaz1990@gmail.comyasunaz1990@gmail.com>
2020-05-13 22:21:56 -0400

    some changes

---
+++ src/lecture0hellojava/HelloWorld.java
@@ -0,0 +1,19 @@
+package lecture0hellojava;
+
+
+// Class is collection of
+// Methods
+public class HelloWorld
+{
+    // Main method
+    // This one method is the only method
+    // where CPU will read and execute the instructions
+    public static void main(String[] args) {
+
+        // Our first instruction to CPU
+        // is to display "Hello World" message
+        // to the Terminal Screen
+        System.out.println("Hello World");
+    }
+
+}
