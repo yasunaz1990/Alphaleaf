f67d26886a28647f6bdf625355059ba296923750
MelisDeniz <melisdilandeniz@yahoo.com>
2020-05-16 18:06:35 -0400

    cleared the src folder

---
+++ src/MelisHW.java
@@ -0,0 +1,24 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.sql.SQLOutput;
+
+public class MelisHW {
+    public static void main(String[] args) {
+
+        System.out.println("Print line");
+        System.out.print("A");
+        System.out.print("B");
+        System.out.println("B");
+        System.out.println();
+        System.out.println("C");
+    }
+
+    
+
+
+
+
+
+
+
+}
--- src/com/practice/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.practice;
-
-public class HelloWorld {
-
-    // Main method - where CPU executes code
-    public static void main(String[] args) {
-        System.out.println("Hello World!!");
-    }
-}
