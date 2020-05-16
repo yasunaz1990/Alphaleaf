a1e040188698180baf1634b91db9b346b1283d26
mmahfuz86 <alhaque.mahfuz@gmail.com>
2020-05-16 14:47:59 -0400

    practice demo May16

--- src/operators/BranchingDemo.java
+++ src/operators/BranchingDemo.java
@@ -1,18 +1,10 @@
-package operators;
-
-import javax.imageio.stream.ImageInputStream;
-import java.util.Scanner;
+package Operators;
 
 public class BranchingDemo {
-    // if statement
-    // if-else statement
-    // chaining if-else statement
 
     public static void main(String[] args) {
 
-        //System.out.println("Select a grade [A, B, C]: ");
-        //Scanner keyboard = new Scanner(System.in);
-        String choice = "B";
+        String choice = "A";
 
         if (choice == "A") {
             System.out.println("You have passed");
@@ -21,17 +13,7 @@ public class BranchingDemo {
         } else if (choice == "C") {
             System.out.println("You have failed");
         } else {
-            System.out.println("This is the end of it!");
+            System.out.println("You did not study");
         }
-
-        System.out.println( true && true);
-        System.out.println( false && true);
-        System.out.println( true & false);
-        System.out.println( false && false);
-        System.out.println(12 % 2);
-        // Looping
-
-        for(int i=0; i<=13; i++)
-            System.out.println(i+ ":\t=\t" + "Apple is the best");
     }
 }
