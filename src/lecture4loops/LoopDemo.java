bb56aa00cfce26f1a8f2e8019eef881a232cbfd4
ZOEY <zhebron09@gmail.com>
2020-05-19 20:30:59 -0400

    updated

--- src/lecture4loops/LoopDemo.java
+++ src/lecture4loops/LoopDemo.java
@@ -23,6 +23,14 @@ public class LoopDemo {
         int sum = 0;
         for(int i = 32; i <= 1000; i++) {
             sum += i;
+
+            if (i % 2 == 0) {
+                continue;
+            }
+
+        }
+        System.out.println("i");
+            }
         }
 
         System.out.println(sum);
---
+++ src/lecture4loops/Zoeypractice.java
@@ -0,0 +1,12 @@
+package lecture4loops;
+
+public class Zoeypractice {
+
+    public static void main(String[] args) {
+
+
+        char[] familyChores = {'Zoey', 'Moe', 'Sanari', 'Kyrie', 'Boosie'}
+
+
+    }
+}
