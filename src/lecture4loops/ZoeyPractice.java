117db255a20678d8e3af45fb6457cdd7e8695868
ZOEY <zhebron09@gmail.com>
2020-05-22 15:22:55 -0400

    fixing

--- src/lecture4loops/ZoeyPractice.java
+++ src/lecture4loops/ZoeyPractice.java
@@ -1,4 +1,17 @@
 package lecture4loops;
 
+import java.util.Arrays;
+
 public class ZoeyPractice {
+
+    public static void main(String[] args) {
+
+        // #1
+        int[] numbers = {90, 40, 89, 86, 45, 65, 78, 87, 45, 47, 45};
+
+        for(int i = 0; i < 10 ; i++)
+
+
+
+    }
 }
